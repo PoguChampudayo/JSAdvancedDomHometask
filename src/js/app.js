5e0828be768efe4040f9cb06fa1ef25d72e1e015
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-02 15:39:29 +0500

    trying to put a goblin

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,23 @@
-
-export default function placeTheMole () {
+export default function placeTheMole() {
   document.addEventListener("DOMContentLoaded", () => {
-    const goblin = document.createElement('div')
-    goblin.className = "goblin"
-    goblin.style.cssText = 'width: 100px; height: 100px; background-image: url("../img/goblin.png")'
+    const goblin = document.createElement("div");
+    goblin.className = "goblin";
+    goblin.style.cssText =
+      'width: 100px; height: 100px; background-image: url("../img/goblin.png")';
     setTimeout(() => {
-      if (document.querySelector('.goblin')) {
-        let currentPlace = document.querySelector('.goblin').parentNode.id
-        goblin.remove()
-        while (true) {
-          let newPlace = Math.round(Math.random() * 15)
-          if (newPlace != currentPlace) {
-            break
-          }
-        }
-        document.querySelectorAll('.cellTable')[newPlace].appendChild(goblin)
+      if (document.querySelector(".goblin")) {
+        let currentPlace = document.querySelector(".goblin").parentNode.id;
+        goblin.remove();
+        let newPlace;
+        do {
+          newPlace = Math.floor(Math.random() * 15);
+        } while (newPlace != currentPlace);
+        document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
+      } else {
+        document
+          .querySelectorAll(".cellTable")
+          [Math.round(Math.random() * 15)].appendChild(goblin);
       }
-      else {
-        document.querySelectorAll('.cellTable')[Math.round(Math.random() * 15)].appendChild(goblin)
-      }
-    }, 2000)
-  }) 
+    }, 2000);
+  });
 }
