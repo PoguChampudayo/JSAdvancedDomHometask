f43ec09f01f64dee948977de79f1489c0c642cda
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-02 14:02:51 +0500

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+
+export default function placeTheMole () {
+  document.addEventListener("DOMContentLoaded", () => {
+    const goblin = document.createElement('div')
+    goblin.className = "goblin"
+    goblin.style.cssText = 'width: 100px; height: 100px; background-image: url("../img/goblin.png")'
+    setTimeout(() => {
+      if (document.querySelector('.goblin')) {
+        let currentPlace = document.querySelector('.goblin').parentNode.id
+        goblin.remove()
+        while (true) {
+          let newPlace = Math.round(Math.random() * 15)
+          if (newPlace != currentPlace) {
+            break
+          }
+        }
+        document.querySelectorAll('.cellTable')[newPlace].appendChild(goblin)
+      }
+      else {
+        document.querySelectorAll('.cellTable')[Math.round(Math.random() * 15)].appendChild(goblin)
+      }
+    }, 2000)
+  }) 
+}
