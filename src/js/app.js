becf5d119f7733d50343a5f7f5102bbd2ee6ae95
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-03 15:06:41 +0500

    test

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const goblin = document.createElement("div");
   goblin.className = "goblin";
+  let newPlace
   setTimeout(() => {
     // if (document.querySelector(".goblin")) {
     //   let currentPlace = document.querySelector(".goblin").parentNode.id;
@@ -14,9 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // } else {
     //   document.querySelectorAll(".cellTable")[Math.round(Math.random() * 15)].appendChild(goblin);
     // }
-    let newPlace = Math.floor(Math.random() * 15);
+    newPlace = Math.floor(Math.random() * 15);
     document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
     console.log("countdown!");
-  }, 2000);
+  }, 1000);
   return true;
 });
