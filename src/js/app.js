a97ad61a055567fc22c63a925eecbee8046ce22d
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-03 15:38:57 +0500

    test

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
   let newPlace;
   setInterval(() => {
     if (document.querySelector(".goblin")) {
-      let currentPlace = Number(document.querySelector(".goblin").parentNode.id);
-      let newPlace;
+      let currentPlace = Number(
+        document.querySelector(".goblin").parentNode.id
+      );
       do {
         newPlace = Math.floor(Math.random() * 15);
-        console.log(newPlace, currentPlace)
+        console.log(newPlace, currentPlace);
       } while (newPlace != currentPlace);
       document.querySelector(".goblin").remove();
       document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
     } else {
-      document.querySelectorAll(".cellTable")[Math.round(Math.random() * 15)].appendChild(goblin);
+      document
+        .querySelectorAll(".cellTable")
+        [Math.round(Math.random() * 15)].appendChild(goblin);
     }
   }, 2000);
   return true;
