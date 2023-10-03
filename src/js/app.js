58fb44a278acbe59a3f39e48f5734b2cc30df899
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-03 13:36:57 +0500

    getting out of func

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,22 @@
-export default function placeTheMole() {
-  document.addEventListener("DOMContentLoaded", () => {
-    const goblin = document.createElement("div");
-    goblin.className = "goblin";
-    goblin.style.cssText =
-      'width: 100px; height: 100px; background-image: url("../img/goblin.png")';
-    setTimeout(() => {
-      if (document.querySelector(".goblin")) {
-        let currentPlace = document.querySelector(".goblin").parentNode.id;
-        goblin.remove();
-        let newPlace;
-        do {
-          newPlace = Math.floor(Math.random() * 15);
-        } while (newPlace != currentPlace);
-        document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
-      } else {
-        document
-          .querySelectorAll(".cellTable")
-          [Math.round(Math.random() * 15)].appendChild(goblin);
-      }
-    }, 2000);
-  });
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const goblin = document.createElement("div");
+  goblin.className = "goblin";
+  goblin.style.cssText =
+    'width: 100px; height: 100px; background-image: url("../img/goblin.png")';
+  setTimeout(() => {
+    if (document.querySelector(".goblin")) {
+      let currentPlace = document.querySelector(".goblin").parentNode.id;
+      goblin.remove();
+      let newPlace;
+      do {
+        newPlace = Math.floor(Math.random() * 15);
+      } while (newPlace != currentPlace);
+      document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
+    } else {
+      document
+        .querySelectorAll(".cellTable")
+        [Math.round(Math.random() * 15)].appendChild(goblin);
+    }
+  }, 2000);
+});
+
