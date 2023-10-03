097b70f7acbfdb07179fd983d3f6a1bc50f817e8
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-03 14:01:34 +0500

    trying things with goblin

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     if (document.querySelector(".goblin")) {
       let currentPlace = document.querySelector(".goblin").parentNode.id;
-      document.querySelector(".goblin").remove();
       let newPlace;
       do {
         newPlace = Math.floor(Math.random() * 15);
-      } while (newPlace != currentPlace);
+        console.log(newPlace)
+      } while (newPlace = currentPlace);
+      document.querySelector(".goblin").remove();
       document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
     } else {
-      document
-        .querySelectorAll(".cellTable")
-        [Math.round(Math.random() * 15)].appendChild(goblin);
+      document.querySelectorAll(".cellTable")[Math.round(Math.random() * 15)].appendChild(goblin);
     }
+    console.log('countdown!')
   }, 2000);
 });
 
