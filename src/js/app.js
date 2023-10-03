c3df003d7c66d853dee841d949220e893322d8a6
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-03 14:52:22 +0500

    test

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const goblin = document.createElement("div");
   goblin.className = "goblin";
   setTimeout(() => {
-    if (document.querySelector(".goblin")) {
-      let currentPlace = document.querySelector(".goblin").parentNode.id;
-      let newPlace;
-      do {
-        newPlace = Math.floor(Math.random() * 15);
-        console.log(newPlace)
-      } while (newPlace = currentPlace);
-      document.querySelector(".goblin").remove();
-      document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
-    } else {
-      document.querySelectorAll(".cellTable")[Math.round(Math.random() * 15)].appendChild(goblin);
-    }
+    // if (document.querySelector(".goblin")) {
+    //   let currentPlace = document.querySelector(".goblin").parentNode.id;
+    //   let newPlace;
+    //   do {
+    //     newPlace = Math.floor(Math.random() * 15);
+    //     console.log(newPlace)
+    //   } while (newPlace = currentPlace);
+    //   document.querySelector(".goblin").remove();
+    //   document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
+    // } else {
+    //   document.querySelectorAll(".cellTable")[Math.round(Math.random() * 15)].appendChild(goblin);
+    // }
+    let newPlace = Math.floor(Math.random() * 15);
+    document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
     console.log('countdown!')
   }, 2000);
 });
