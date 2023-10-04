6d90405c93fa57fa64f3115705a464c3d8e6fd2f
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-04 09:34:56 +0500

    finalizing

--- src/js/app.js
+++ src/js/app.js
@@ -10,26 +10,12 @@ function getRandomInt(max, min, outNumber) {
 document.addEventListener("DOMContentLoaded", () => {
   const goblin = document.createElement("img");
   goblin.className = "goblin";
-  // let newPlace;
+  document.querySelectorAll(".cellTable")[Math.round(Math.random() * 15)].appendChild(goblin);
   setInterval(() => {
-    if (document.querySelector(".goblin")) {
-      let currentPlace = Number(
-        document.querySelector(".goblin").parentNode.id
-      );
-      let newPlace = getRandomInt(15, 0, currentPlace);
-      // do {
-      //   newPlace = Math.floor(Math.random() * 15);
-      //   console.log(newPlace, currentPlace);
-      // } while (newPlace = currentPlace);
-      // newPlace = Math.floor(Math.random() * 15);
-      console.log(newPlace, currentPlace);
-      document.querySelector(".goblin").remove();
-      document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
-    } else {
-      document
-        .querySelectorAll(".cellTable")
-        [Math.round(Math.random() * 15)].appendChild(goblin);
-    }
+    let currentPlace = Number(document.querySelector(".goblin").parentNode.id);
+    let newPlace = getRandomInt(15, 0, currentPlace);
+    document.querySelector(".goblin").remove();
+    document.querySelectorAll(".cellTable")[newPlace].appendChild(goblin);
   }, 2000);
   return true;
 });
