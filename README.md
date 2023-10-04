84ba03e8f636c2ee977ccb985fc59d61117e6995
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-10-04 09:30:40 +0500

    test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ![CI](https://github.com/Poguchampudayo/JSAdvancedDOMHometask/actions/workflows/web.yml/badge.svg)
+
+https://poguchampudayo.github.io/JSAdvancedDomHometask/
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,22 @@
 function getRandomInt(max, min, outNumber) {
-  let result = Math.floor(Math.random() * (max - min) + min)
+  let result = Math.floor(Math.random() * (max - min) + min);
   if (result == outNumber) {
-    return getRandomInt(max, min, outNumber)
-  }
-  else {
-    return result
+    return getRandomInt(max, min, outNumber);
+  } else {
+    return result;
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const goblin = document.createElement("div");
+  const goblin = document.createElement("img");
   goblin.className = "goblin";
   // let newPlace;
   setInterval(() => {
     if (document.querySelector(".goblin")) {
-      let currentPlace = Number(document.querySelector(".goblin").parentNode.id);
-      let newPlace = getRandomInt(15, 0, currentPlace)
+      let currentPlace = Number(
+        document.querySelector(".goblin").parentNode.id
+      );
+      let newPlace = getRandomInt(15, 0, currentPlace);
       // do {
       //   newPlace = Math.floor(Math.random() * 15);
       //   console.log(newPlace, currentPlace);
